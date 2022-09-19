a32ca9bc29ca9c656450dc44cf287f8a5dcf2b57
EvgeniyLV <zhenia.larionoff2015@yandex.ru>
2022-09-19 19:34:12 +0300

    контент для secure

--- frontend/src/components/Secure.vue
+++ frontend/src/components/Secure.vue
@@ -1,6 +1,13 @@
 <template>
-  <div>
-    <h1>This page is protected by auth</h1>
+  <div class="card w-50 mt-5 mx-auto">
+    <h5 class="card-header">Featured</h5>
+    <div class="card-body">
+      <h5 class="card-title">Special title treatment</h5>
+      <p class="card-text">
+        With supporting text below as a natural lead-in to additional content.
+      </p>
+      <a href="#" class="btn btn-primary">Go somewhere</a>
+    </div>
   </div>
 </template>
 <script>

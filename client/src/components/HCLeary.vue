f4a2e39e734eda21aed5af1eba6394d84d02f81f
EvgeniyLV <zhenia.larionoff2015@yandex.ru>
2023-07-05 20:26:01 +0300

    refactor: переход на composition api в компонете HCLeary.vue

--- client/src/components/HCLeary.vue
+++ client/src/components/HCLeary.vue
@@ -5,159 +5,84 @@
   </div>
 </template>
 
-<script>
-import Highcharts from "highcharts";
-import HighchartsMore from "highcharts/highcharts-more";
-HighchartsMore(Highcharts);
-export default {
-  name: "HighchartsBlock",
-  props: {
-    options: {
-      type: Array,
-      required: true,
-    },
-    arr: {
-      type: Array,
-      required: true,
-    },
+<script setup>
+import { watch, toRefs, ref, nextTick } from "vue";
+const props = defineProps({
+  dataArr: Array,
+});
+
+const { dataArr } = toRefs(props);
+
+const chartOptions = ref({
+  chart: {
+    polar: true,
+    type: "line",
   },
-  data() {
-    return {
-      //counter: 2,
-      chartOptions: Highcharts.merge(this.options, {
-        chart: {
-          polar: true,
-          type: "line",
-          //backgroundColor: "#FCFFC5",
-        },
-        accessibility: { enabled: false },
+  accessibility: { enabled: false },
 
-        title: {
-          text: "",
-          //y: 0,
-          //x: -80,
-          //x: 0,
-        },
-
-        subtitle: {
-          //text: "Н-нестабильность, Э-экстраверсия, С-стабильность, И-интроверсия",
-          //y: 0,
-          //y: -0.3,
-        },
-
-        pane: {
-          size: "100%",
-        },
-
-        // xAxis: {
-        //   //visible: false,
-        //   labels: {
-        //     enabled: false,
-        //     X: 0,
-        //     y: 0,
-        //   },
-        //   categories: ["Н", "Э", "С", "И"],
-        //   tickmarkPlacement: "on",
-        //   lineWidth: 0,
-        // },
-        xAxis: {
-          categories: [
-            "Властный",
-            "Великодушный",
-            "Сотрудничающий",
-            "Зависимый",
-            "Покорный",
-            "Недоверчивый",
-            "Агрессивный",
-            "Независимый",
-          ],
-          tickmarkPlacement: "on",
-          lineWidth: 0,
-        },
-
-        yAxis: {
-          gridLineInterpolation: "polygon",
-          lineWidth: 0,
-          min: 0,
-        },
-
-        // tooltip: {
-        //   shared: true,
-        //   pointFormat:
-        //     '<span style="color:{series.color}">{series.name}: <b>${point.y:,.0f}</b><br/>',
-        // },
-
-        legend: {
-          enabled: false,
-          align: "right",
-          verticalAlign: "middle",
-          layout: "vertical",
-        },
-
-        series: [
-          {
-            name: "Allocated Budget",
-            data: this.arr,
-            pointPlacement: "on",
-            color: "#ffff01",
-            lineColor: "#ffff01",
-          },
-          // {
-          //   name: "Actual Spending",
-          //   data: [50, 39, 42, 31, 26, 14, 46, 13],
-          //   pointPlacement: "on",
-          // },
-        ],
-
-        // responsive: {
-        //   rules: [
-        //     {
-        //       condition: {
-        //         maxWidth: 500,
-        //       },
-        //       chartOptions: {
-        //         legend: {
-        //           align: "center",
-        //           verticalAlign: "bottom",
-        //           layout: "horizontal",
-        //         },
-        //         pane: {
-        //           size: "70%",
-        //         },
-        //       },
-        //     },
-        //   ],
-        // },
-      }),
-    };
+  title: {
+    text: "",
   },
-  watch: {
-    options: {
-      handler(newOpt) {
-        //console.log(newOpt);
-        this.chartOptions.series = newOpt;
-      },
-      deep: true,
+  pane: {
+    size: "100%",
+  },
+  xAxis: {
+    categories: [
+      "Властный",
+      "Великодушный",
+      "Сотрудничающий",
+      "Зависимый",
+      "Покорный",
+      "Недоверчивый",
+      "Агрессивный",
+      "Независимый",
+    ],
+    tickmarkPlacement: "on",
+    lineWidth: 0,
+  },
+
+  yAxis: {
+    gridLineInterpolation: "polygon",
+    lineWidth: 0,
+    min: 0,
+  },
+
+  legend: {
+    enabled: false,
+    align: "right",
+    verticalAlign: "middle",
+    layout: "vertical",
+  },
+
+  series: [
+    {
+      name: "Allocated Budget",
+      data: [],
+      pointPlacement: "on",
+      color: "#ffff01",
+      lineColor: "#ffff01",
     },
-  },
-  //   computed: {
-  //     getOptionsData() {
-  //       return this.optionsData;
-  //     },
-  //   },
-};
+  ],
+});
+
+// watch(
+//   dataArr,
+//   (newValue) => {
+//     chartOptions.value.series[0].data = newValue;
+//   },
+//   { deep: true }
+// );
+
+setTimeout(() => {
+  chartOptions.value.series[0].data = dataArr;
+}, 0);
+
 </script>
 <style lang="scss" scoped>
 .high-charts {
   position: relative;
-  //display: flex;
-  //align-items: center;
-  //justify-content: center;
   width: 100%;
   height: 100%;
-  @media (max-width: 1040px) {
-    //width: 100%;
-  }
 
   .leary-circle {
     position: absolute;
@@ -172,7 +97,6 @@ export default {
 }
 :deep(.hc) {
   width: 100%;
-  //position: absolute;
   background: transparent;
   svg rect {
     fill: none;

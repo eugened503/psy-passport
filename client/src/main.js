d114e476554a32309102adb207cc15a46ef9b96a
EvgeniyLV <zhenia.larionoff2015@yandex.ru>
2023-07-05 20:31:24 +0300

    fix: Исправление загрузки highcharts

--- client/src/main.js
+++ client/src/main.js
@@ -5,8 +5,6 @@ import store from "./store/index.js";
 import "./main.scss";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap";
-import Highcharts from "highcharts";
-import hcMore from "highcharts/highcharts-more";
 import HighchartsVue from "highcharts-vue";
 import { library, dom } from "@fortawesome/fontawesome-svg-core";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
@@ -17,7 +15,6 @@ import { far } from "@fortawesome/free-regular-svg-icons";
 library.add(fas, far, fab);
 dom.watch();
 library.add(fas, far, fab);
-hcMore(Highcharts);
 
 const app = createApp(App);
 app

148d04a1c76cd2c3ce427a2ece453b2741b567d1
EvgeniyLV <zhenia.larionoff2015@yandex.ru>
2023-07-08 16:10:18 +0300

    refactor: удалены комментарии

--- client/src/store/user.js
+++ client/src/store/user.js
@@ -1,11 +1,9 @@
 import axios from "axios";
 import router from "@/router";
-//import * as res from "./results";
 
 export default {
   namespaced: true,
   state: {
-    //status: "",
     token: localStorage.getItem("token") || null,
     user: null,
     error: null,
@@ -14,25 +12,17 @@ export default {
   actions: {
     register({ commit }, user) {
       return new Promise((resolve, reject) => {
-        //commit("request");
         axios({
           url: "http://localhost:3000/signup",
           data: user,
           method: "POST",
         })
           .then((resp) => {
-            //console.log("resp", resp);
-            //const token = resp.data.token;
-            //const user = resp.data.user;
-            //localStorage.setItem("token", token);
-            //axios.defaults.headers.common["Authorization"] = token;
-            //commit("register_success");
             resolve(resp);
             router.push({ name: "login" });
           })
           .catch((err) => {
             commit("error", err.response.data.message);
-            //localStorage.removeItem("token");
             reject(err.response.data.message);
           });
       });
@@ -40,7 +30,6 @@ export default {
 
     login({ commit }, user) {
       return new Promise((resolve, reject) => {
-        //commit("request");
         axios({
           url: "http://localhost:3000/signin",
           data: user,
@@ -49,22 +38,17 @@ export default {
           .then((resp) => {
             const token = resp.data.token;
             localStorage.setItem("token", token);
-            //axios.defaults.headers.common["Authorization"] = token;
             commit("login_success", token);
             resolve(resp);
             router.push({ name: "home" });
           })
           .catch((err) => {
-            console.log(err);
-            //console.log(err.response.data.validation.body.message);
-            //console.log(err.response.status);
+            localStorage.removeItem("token");
             if (err.response.status === 400) {
               commit("error", "Пользователь с таким email не найден");
-              //localStorage.removeItem("token");
               reject("Пользователь с таким email не найден");
             } else {
               commit("error", err.response.data.message);
-              //localStorage.removeItem("token");
               reject(err.response.data.message);
             }
           });
@@ -72,14 +56,7 @@ export default {
     },
 
     logout({ commit, getters }) {
-      // return new Promise((resolve) => {
-      //   commit("logout");
-      //   localStorage.removeItem("token");
-      //   //delete axios.defaults.headers.common["Authorization"];
-      //   resolve();
-      // });
       return new Promise((resolve, reject) => {
-        //commit("request");
         axios({
           url: "http://localhost:3000/exit",
           method: "POST",
@@ -89,24 +66,21 @@ export default {
           },
         })
           .then((resp) => {
-            //console.log(resp);
             commit("logout");
             localStorage.removeItem("token");
             resolve(resp);
             router.push({ name: "home" });
           })
-          .catch((err) => {
-            commit("error", err.response.data.message);
-            //localStorage.removeItem("token");
-            reject(err.response.data.message);
-          });
+          // .catch((err) => {
+          //   commit("error", err.response.data.message);
+          //   localStorage.removeItem("token");
+          //   reject(err.response.data.message);
+          // });
       });
     },
 
     profile({ commit, getters }) {
-      //console.log("profile");
       return new Promise((resolve, reject) => {
-        //commit("request");
         axios({
           url: "http://localhost:3000/users/me",
           method: "GET",
@@ -116,7 +90,6 @@ export default {
           },
         })
           .then((resp) => {
-            //const token = resp.data.token;
             const user = resp.data;
             commit("user_success", user);
             resolve(resp);
@@ -124,7 +97,6 @@ export default {
               router.isReady() &&
               router.currentRoute.value.path === "/login"
             ) {
-              //console.log("123");
               router.push("/");
             }
           })
@@ -139,36 +111,23 @@ export default {
   },
 
   mutations: {
-    // request(state) {
-    //   //state.status = "loading";
-    // },
-
-    // register_success(state) {
-    //   //state.status = "success";
-    // },
-
     login_success(state, token) {
-      //state.status = "success";
       state.token = token;
     },
 
     logout(state) {
-      //state.status = "";
       state.token = null;
     },
 
     user_success(state, user) {
-      //console.log("user_success");
-      //state.status = "success";
       state.user = user;
     },
 
     error(state, error) {
-      //state.status = "error";
       state.error = error;
     },
-    clearError(state) {
-      //state.status = "error";
+
+    clear_error(state) {
       state.error = null;
     },
   },
@@ -176,7 +135,6 @@ export default {
   getters: {
     getUser: (state) => state.user,
     isLoggedIn: (state) => !!state.token,
-    //authStatus: (state) => state.status,
     getToken: (state) => state.token,
     getError: (state) => state.error,
   },

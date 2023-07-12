4799c3b7cd383c9967404d7767ce3a2729a0c72e
EvgeniyLV <zhenia.larionoff2015@yandex.ru>
2023-07-12 19:55:52 +0300

    feat: Добавлен компонент DropdownMenu

---
+++ client/src/components/DropdownMenu.vue
@@ -0,0 +1,87 @@
+<template>
+  <nav class="dropdown-menu">
+    <button class="dropdown-menu__button" @click="isOpen">Тесты</button>
+    <Transition name="fade">
+      <ul class="dropdown-menu__body" v-if="show">
+        <li>
+          <router-link
+            class="dropdown-menu__item ellipsis"
+            @click="isOpen"
+            :to="{ name: 'eysenck' }"
+            >Тест&nbsp;Айзенка</router-link
+          >
+        </li>
+        <li>
+          <router-link
+            class="dropdown-menu__item ellipsis"
+            @click="isOpen"
+            :to="{ name: 'leary' }"
+            >Тест Лири</router-link
+          >
+        </li>
+        <li>
+          <router-link
+            class="dropdown-menu__item ellipsis"
+            @click="isOpen"
+            :to="{ name: 'shmishek' }"
+            >Тест Шмишека и Леонгарда</router-link
+          >
+        </li>
+      </ul>
+    </Transition>
+  </nav>
+</template>
+
+<script setup>
+import { ref } from "vue";
+
+const show = ref(false);
+const isOpen = () => (show.value = !show.value);
+</script>
+
+<style lang="scss" scoped>
+.dropdown-menu {
+  position: relative;
+
+  &__body {
+    position: absolute;
+    max-width: 100px;
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
+    padding: 10px;
+    background-color: $clr-white;
+    top: 50px;
+    border-radius:  0 0 10px 10px;
+  }
+
+  &__button,
+  &__item {
+    color: $clr-manatee;
+    transition: color 0.2s ease-in-out;
+    &:hover {
+      color: $clr-slate-grey;
+    }
+  }
+
+  &__button {
+    display: flex;
+    @include link-item;
+  }
+
+  &__item {
+    display: block;
+    font-size: 14px;
+  }
+}
+
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.3s ease;
+}
+
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+}
+</style>

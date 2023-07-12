55cf08eb21fc38a4ddce36d13f4b163408ea7cf9
EvgeniyLV <zhenia.larionoff2015@yandex.ru>
2023-07-12 19:50:03 +0300

    feat: Добавлены scss-миксины

--- client/vite.config.js
+++ client/vite.config.js
@@ -20,7 +20,7 @@ export default defineConfig({
   css: {
     preprocessorOptions: {
       scss: {
-        additionalData: `@import "./src/assets/scss/variables.scss";`,
+        additionalData: `@import "./src/assets/scss/index.scss";`,
       },
     },
   },

1c4820a218a3bba89a501ffbbf184c80299062ef
EvgeniyLV <zhenia.larionoff2015@yandex.ru>
2023-07-12 19:52:20 +0300

    refactor: Удален Bootstrap

--- client/src/main.js
+++ client/src/main.js
@@ -1,26 +1,16 @@
+import "./main.scss";
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import { createPinia } from "pinia";
-import "./main.scss";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap";
 import HighchartsVue from "highcharts-vue";
-import { library, dom } from "@fortawesome/fontawesome-svg-core";
-import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
-import { fas } from "@fortawesome/free-solid-svg-icons";
-import { fab } from "@fortawesome/free-brands-svg-icons";
-import { far } from "@fortawesome/free-regular-svg-icons";
 
-library.add(fas, far, fab);
-dom.watch();
-library.add(fas, far, fab);
+
 const pinia = createPinia();
-
 const app = createApp(App);
+
 app
   .use(router)
   .use(pinia)
   .use(HighchartsVue)
-  .component("font-awesome-icon", FontAwesomeIcon)
   .mount("#app");

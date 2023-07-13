3ec1d82d2c3d3cb215b5a35f5794e29cf2304dff
EvgeniyLV <zhenia.larionoff2015@yandex.ru>
2023-07-13 15:22:07 +0300

    feat: Добавлен лейаут TestsLayout

---
+++ client/src/layouts/TestsLayout.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="tests">
+    <router-view />
+  </div>
+</template>
+<script setup>
+import { onBeforeMount } from "vue";
+import { useStoreResults } from "@/stores/storeResults";
+
+const { loadResults } = useStoreResults();
+
+onBeforeMount(() => loadResults());
+</script>
+<style lang="scss"></style>

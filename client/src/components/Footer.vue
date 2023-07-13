b905edd2cfb1e6983fcfe7edafd37617dfd3ca8a
EvgeniyLV <zhenia.larionoff2015@yandex.ru>
2023-07-13 15:19:48 +0300

    feat: Добавлен компонент Footer

---
+++ client/src/components/Footer.vue
@@ -0,0 +1,19 @@
+<template>
+  <footer>
+    <p>Copyright 2023 Eugene.</p>
+  </footer>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped>
+footer {
+  display: flex;
+  background-color: $clr-white;
+  justify-content: center;
+  padding: 15px 0;
+  p {
+    color: $clr-manatee;
+  }
+}
+</style>

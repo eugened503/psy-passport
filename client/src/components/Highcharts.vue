f538c06502b5eb9499547a60fb0837ef724ffca7
EvgeniyLV <zhenia.larionoff2015@yandex.ru>
2023-07-15 17:39:14 +0300

    feat: Добавлены слот и пропс

--- client/src/components/Highcharts.vue
+++ client/src/components/Highcharts.vue
@@ -1,69 +1,19 @@
 <template>
   <div class="high-charts">
-    <div class="eysenck-circle"></div>
+    <slot name="background"></slot>
     <highcharts class="highcharts" :options="chartOptions" />
   </div>
 </template>
 
 <script setup>
-import { toRefs, ref, onMounted, reactive } from "vue";
+import { toRefs, onMounted } from "vue";
 
 const props = defineProps({
   dataArr: Array,
+  chartOptions: Object,
 });
 
-const { dataArr } = toRefs(props);
-
-const chartOptions = ref({
-  chart: {
-    polar: true,
-    type: "line",
-  },
-
-  accessibility: { enabled: false },
-
-  title: {
-    text: "",
-  },
-
-  pane: {
-    size: "100%",
-  },
-
-  xAxis: {
-    labels: {
-      enabled: false,
-      X: 0,
-      y: 0,
-    },
-    categories: ["Н", "Э", "С", "И"],
-    tickmarkPlacement: "on",
-    lineWidth: 0,
-  },
-
-  yAxis: {
-    gridLineInterpolation: "polygon",
-    lineWidth: 0,
-    min: 0,
-  },
-
-  legend: {
-    enabled: false,
-    align: "center",
-    verticalAlign: "middle",
-    layout: "vertical",
-  },
-
-  series: [
-    {
-      type: "area",
-      name: "Area",
-      data: [0, 0, 0, 0],
-      pointPlacement: "on",
-      maxWidth: 100,
-    },
-  ],
-});
+const { dataArr, chartOptions } = toRefs(props);
 
 onMounted(() => {
   chartOptions.value.series[0].data = dataArr;
@@ -72,15 +22,6 @@ onMounted(() => {
 <style lang="scss" scoped>
 .high-charts {
   position: relative;
-  .eysenck-circle {
-    position: absolute;
-    width: 100%;
-    height: 100%;
-    z-index: 1;
-    background: url(../assets/images/eysenck-circle3.svg) no-repeat;
-    background-position: center;
-    background-size: contain;
-  }
 }
 </style>
 
